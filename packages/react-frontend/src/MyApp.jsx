bdf19fa2017f94e79126ee3f5462348788068dd1
Vladimir Gupenko <vgupenko@calpoly.edu>
2025-04-04 10:47:40 -0700

    If you check back on your live environment, you’ll see the Table loaded in WORKS

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,11 +1,13 @@
 // src/MyApp.jsx
 import React from "react";
+import Table from "./Table";
 
 function MyApp() {
   return (
-    <div>
-      <h1>Hello, React!</h1>
+    <div className="container">
+      <Table />
     </div>
   );
 }
+
 export default MyApp;
