6208ac72a848a56f32867bd87738b63e92d5cf4c
Vladimir Gupenko <vgupenko@calpoly.edu>
2025-04-18 10:49:59 -0700

    Updated table to show user IDs.

--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -3,6 +3,7 @@ function TableHeader() {
   return (
     <thead>
       <tr>
+        <th>ID</th>
         <th>Name</th>
         <th>Job</th>
       </tr>
@@ -14,6 +15,7 @@ function TableBody(props) {
   const rows = props.characterData.map((row, index) => {
     return (
       <tr key={index}>
+  <td>{row.id}</td>
   <td>{row.name}</td>
   <td>{row.job}</td>
   <td>
